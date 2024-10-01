f11443fe674cd259818f03c39835fe5e44896b28
Julia Sanchis <jursanchis@gmail.com>
2024-10-01 13:50:56 +0200

    styling home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
-import { BsFillPostcardFill } from "react-icons/bs";
-
 import "./App.css";
 
 function App() {
   return (
     <>
       <div className="intro">
-        <img className="photo" src="./cv-photo.png" />
-        <h1>
-          <BsFillPostcardFill style={{ color: "yellow", fontSize: "2rem" }} />
-          ¡Hola!
-        </h1>
-        <p>Me llamo Julia Sanchis y soy de Sevilla.</p>
-        <p>Software Developer</p>
+        <img className="intro-photo" src="./cv-photo.png" />
+        <div className="intro-text">
+          <h1>
+            {/* <GrPersonalComputer style={{ fontSize: "2.5rem" }} /> */}
+            ¡Hola!
+          </h1>
+          <p>
+            Me llamo Julia Sanchis, soy de Sevilla (España) y soy{" "}
+            <strong>Software Developer</strong>
+          </p>
+        </div>
+        <div className="intro-links">
+          <a href="https://www.github.com/bonirot">GitHub</a>
+        </div>
       </div>
     </>
   );
